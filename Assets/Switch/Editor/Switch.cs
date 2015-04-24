61ff926d5cad9a2802c9e1577a9e51448ff14088
William Kwan <william@replayism.com>
2015-04-24 14:59:43 -0700

    init

---
+++ Assets/Switch/Editor/Switch.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using UnityEditorInternal;
+
+public class Switch : EditorWindow {
+	
+	[MenuItem("Switch/Switch! %e")]
+	static void SwitchToIde() {
+//		AssetDatabase.OpenAsset(this);
+		UnityEditorInternal.InternalEditorUtility.OpenFileAtLineExternal("", 0);
+	}
+}
